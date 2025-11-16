2fea2fbc0e0c75d1ffac37f2b662c2d87a5e8ae6
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-11-15 23:35:10 -0300

    cambio var

--- src/app/components/Actividades.js
+++ src/app/components/Actividades.js
@@ -4,18 +4,22 @@ import { useState, useEffect } from "react";
 import { useAppContext } from "@/app/context/AppContext";
 import Image from "next/image";
 import Loading from "./Loading";
+import Link from "next/link";
 
 const Actividades = () => {
   const { actividades, fetchActividades } = useAppContext();
   const [loading, setLoading ] = useState(true);
 
-  useEffect(()=>{
-    const loadActividades = async() =>{
-      await fetchActividades();
-      setLoading(false);
-    }
-      loadActividades();
-  },[]);
+  useEffect(() => {
+      const loadData = async () => {
+          if (!actividades.length) {
+              await fetchActividades();
+          }
+          setLoading(false);
+      };
+      loadData();
+  }, [actividades, fetchActividades]);
+
 
   if (loading) return <Loading />;
   if (!actividades.length) return <p className="text-center mt-10 text-gray-600">No hay actividades disponibles.</p>;
@@ -23,10 +27,11 @@ const Actividades = () => {
   return (
     <div className="bg-[#000000] min-h-screen py-10 px-6">
       <h3 className="text-3xl text-left pl-30 m-5 font-bold">Actividades</h3>
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
         {actividades.map((actividad) => (
+        <Link key={actividad._id} href={`/categoriasActividades/${actividad._id}`}>
           <div
-            key={actividad._id}
             className="relative cursor-pointer bg-black rounded-2xl shadow-md hover:shadow-xl transition-transform duration-300 hover:scale-105 active:scale-95 overflow-hidden flex flex-col items-center text-center"
           >
             <Image
@@ -43,10 +48,11 @@ const Actividades = () => {
                 {actividad.ActividadNombre}
               </h3>
             </div>
-
           </div>
+          </Link>
+
         ))}
-      </div>
+      </div>    
     </div>
   );
 };
--- src/app/context/AppContext.js
+++ src/app/context/AppContext.js
@@ -4,23 +4,26 @@ import { createContext, useContext, useState } from "react";
 import axios from "axios";
 
 const AppContext = createContext();
-const API_URL = "http://localhost:4000"; // tu backend
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const AppProvider = ({ children }) => {
   const [actividades, setActividades] = useState([]); //estas son las que necesito del back
   const [predicas, setPredicas] = useState([]);
   const [searchTerm, setSearchTerm] = useState(""); //busqueda
 
-  // ACTIVIDADES
+   // ACTIVIDADES
   const fetchActividades = async () => { //fetch es una fucncion asincronica
     try {
       const res = await axios.get(`${API_URL}/actividades`); //agarro acitivdadeds del link de api
       // link img
       const dataWithImages = res.data.map(act => ({
-      ...act,//... me trae todas las rtas del objeto, en este caso de las portadas
+      ...act,//... me trae todas las rtas del objeto, en este caso de las portadas, imgs y acts
       Portada: act.Portada 
-        ? `http://localhost:4000/${act.Portada}` 
-        : ""
+        ? `${API_URL}/${act.Portada}` 
+        : "",
+      ImagenesActividades: act.ImagenesActividades
+      ? act.ImagenesActividades.map(img => `${API_URL}/${img}`)  
+      : [],    
     }));
 
       setActividades(dataWithImages);
@@ -28,7 +31,7 @@ export const AppProvider = ({ children }) => {
       console.log("Error fetching actividades:", error);
       setActividades([]);
     }
-  };  
+  };
   
   const fetchPredicas = async () => {
     try{
