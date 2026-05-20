402b239091107315c3588e7c560a6f34b0591879
Agustina Ferraro <ferraroagustina25@gmail.com>
2026-05-20 18:03:17 -0300

    fix: BackButton sin mb-4, pegado al margen del componente

--- src/app/components/BackButton.js
+++ src/app/components/BackButton.js
@@ -8,7 +8,7 @@ const BackButton = () => {
   return (
     <button
       onClick={() => router.back()}
-      className="cursor-pointer flex items-center justify-center text-white hover:text-gray-300 transition-all duration-200 mb-4 w-14 h-14 hover:scale-125 active:scale-90"
+      className="cursor-pointer flex items-center justify-center text-white hover:text-gray-300 transition-all duration-200 w-14 h-14 hover:scale-125 active:scale-90"
     >
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-9 h-9">
         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
