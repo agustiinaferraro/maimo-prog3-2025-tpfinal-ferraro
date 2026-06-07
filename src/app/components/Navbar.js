473ff6a3ebffd1dbd1e8f11fd0d63341aea241a3
Agustina Ferraro <ferraroagustina25@gmail.com>
2026-06-06 21:20:02 -0300

    fix: nav bg-black/40 backdrop-blur-sm para que no quede sin fondo

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -72,7 +72,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="fixed top-0 left-0 w-full z-20">
+      <div className="fixed top-0 left-0 w-full z-20 bg-black/40 backdrop-blur-sm">
         <div className="flex items-center justify-between flex-wrap text-white px-6 lg:px-10 h-[72px] relative">
           
           <Link href="/">
@@ -95,7 +95,7 @@ const Navbar = () => {
           <nav
             className={`w-full lg:w-auto ${
               menuOpen ? "block absolute top-[72px] left-0 z-10" : "hidden"
-            } lg:flex lg:items-center lg:gap-6 lg:ml-auto lg:mt-0 p-4 lg:p-0`}
+            } lg:flex lg:items-center lg:gap-6 lg:ml-auto lg:mt-0 bg-black/40 backdrop-blur-sm rounded-b-xl p-4 lg:p-0`}
           >
             <ul className="flex flex-col lg:flex-row gap-2 lg:gap-14 text-gray-300 px-2 lg:px-0 items-stretch lg:items-center">
 
